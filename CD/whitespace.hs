96abebf02b688e5445d15483799472c087a21f27
arnav <arnav.jose@gmail.com>
2021-11-04 17:30:03 +0530

    fold remove whitespace

--- CD/whitespace.hs
+++ CD/whitespace.hs
@@ -7,11 +7,11 @@ main = do
     else do
         let (a: b: xs) = args
         inp <- readFile a
-        let (outh,_) =  foldr mango ([],' ') inp-- removeWS (reverse inp) []
+        let (outh,_) =  foldr removeWS ([],' ') inp
         writeFile b outh
 
-mango :: Char -> ([Char], Char) -> ([Char], Char)
-mango x (acc, last)
+removeWS :: Char -> ([Char], Char) -> ([Char], Char)
+removeWS x (acc, last)
     | isWhitespace x && isWhitespace last = (acc, last)
     | isWhitespace x = (' ':acc, x)
     | otherwise = (x:acc, x)
