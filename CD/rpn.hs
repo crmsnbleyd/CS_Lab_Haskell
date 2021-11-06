65fb30967f1b3e5533e7661c65faba6447a2b070
arnav <arnav.jose@gmail.com>
2021-11-06 16:14:14 +0530

    reverse polish notation calculator

---
+++ CD/rpn.hs
@@ -0,0 +1,22 @@
+main :: IO ()
+main = do
+    inp <- getLine
+    print . rpn $ inp
+
+rpn :: String -> Float
+rpn str = helper (words str) []
+
+helper :: [String] -> [String] -> Float
+helper [] [x] = read x :: Float
+helper [] _ = error "malformed expression"
+
+helper (op:rest) (x:y:stack)
+    | op == "+" = helper rest (show (a+b): stack)
+    | op == "-" = helper rest (show (a-b): stack)
+    | op == "*" = helper rest (show (a*b): stack)
+    | op == "/" = helper rest (show (a/b): stack)
+    | op == "^" = helper rest (show (a**b): stack)
+    where a = read x :: Float
+          b = read y :: Float
+
+helper (num:rest) stack = helper rest (num:stack)
